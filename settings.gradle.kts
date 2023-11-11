9c9a57e392adea102711bf4ed6d04bb7ce363fac
Matej Persic <matpersic@gmail.com>
2023-11-11 14:00:09 +0100

    project assets added

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LocalNotificationsApp"
+
+include(":androidApp")
+include(":shared")
+
